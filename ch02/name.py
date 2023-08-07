cf4704f1c7eafd7f1c13d8af67f272f86804eb8b
Vadym Gyrenko <vadim.girenko@idealscorp.com>
2023-08-07 23:40:10 +0300

    working with variables

---
+++ ch02/name.py
@@ -0,0 +1,22 @@
+name = "ada lovelace"
+print(name.title())
+
+name = "ADA lOveLACE"
+print(name.title())
+
+name = "ada lovelace"
+print(name.upper())
+
+name = "ADA LOVELACE"
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name= first_name + " " + last_name
+print("Hello, " + full_name.title() + ", I'm very glad to meet you!")
+
+first_name = "ada"
+last_name = "lovelace"
+full_name= first_name + " " + last_name
+message = "Hello, " + full_name.title() + ", I'm very glad to meet you!\nIn this case I used the concatenation"
+print(message)
