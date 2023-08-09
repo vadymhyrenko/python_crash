b698d65efd35a0810af7902a0f40eff7652b4d05
Vadym Gyrenko <vadim.girenko@idealscorp.com>
2023-08-09 19:49:33 +0300

    bicycles added

---
+++ ch03/bicycles.py
@@ -0,0 +1,8 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[1].title())
+print(bicycles[0].upper())
+print(bicycles[-1]) # [-1] is the last element of the list
+
+message = "My first bicycle was " + bicycles[2].title() + "."
+print(message)
