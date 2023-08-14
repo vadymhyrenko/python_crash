d1abbb3573d62e1b98505fb3a30d7e1168a39bc2
Vadym Gyrenko <vadim.girenko@idealscorp.com>
2023-08-14 23:05:29 +0300

    ch04 added with magicians

---
+++ ch04/magicians.py
@@ -0,0 +1,5 @@
+magicians = ["alice", "bob", "harold", "arnold"]
+for magician in magicians:
+    print(magician.title() + " is a great wizard!" )
+    print("Can't wait to see your next show, " + magician.title() + "!\n" )
+print("See ya, everybody!")
