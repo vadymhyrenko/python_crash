047d8f1a872b25d136e2080afac72ee4e81fd993
Vadym Hyrenko <vadym.hyrenko@gmail.com>
2023-08-15 23:34:50 +0300

    squares added

--- ch04/numbers.py
+++ ch04/numbers.py
@@ -1,3 +1,23 @@
-numbers = [1, 2, 3, 4, 5]
+numbers = [1,2,3,4,5]
 for number in numbers:
+    print(number)
+
+for value in range (6,10):
+    print(value)
+
+print("\nNow lets use list with range methods:\n")
+
+numbers = list(range(1,11))
+print(numbers)
+
+for number in numbers:
+    print(number)
+
+digits = [1,3,4,5,7,9]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+even_numbers = list(range(2,11,2))
+for number in even_numbers:
     print(number)
---
+++ ch04/squares.py
@@ -0,0 +1,13 @@
+squares = []
+for value in range (1,11):
+    square = value**2
+    squares.append(square)
+print(squares)
+
+squares = []
+for value in range (1,11):
+    squares.append(value**2)
+print(squares)
+
+squares = [value**2 for value in range(1,11)] # comprehension list
+print(squares)
