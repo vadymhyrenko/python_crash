5b14e65db32a07340b5c515fb2be17e458b3e388
Vadym Hyrenko <vadym.hyrenko@gmail.com>
2023-12-21 23:20:34 +0200

    ch05 added

---
+++ ch05/cars.py
@@ -0,0 +1,8 @@
+cars = ("bmw", "ford", "subaru", "mazda")
+for car in cars:
+    if car == "bmw": # conditional test True
+        print(car.upper())
+    elif car == "honda":  # conditional test False
+        print("Howdy?")
+    else:
+        print(car.title())
---
+++ ch05/magic_number.py
@@ -0,0 +1,6 @@
+print("try to guess the number, please")
+number = input("please enter a number:\n")
+if number == "five":
+    print(f"the {number} you've entered is correct")
+else:
+    print(f"{number} you've entered is wrong")
