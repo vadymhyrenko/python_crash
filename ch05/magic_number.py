379b95365db70a8db3cef37460df85ce993e9c7d
Vadym Hyrenko <vadym.hyrenko@gmail.com>
2024-01-03 23:59:14 +0200

    voting added with conditionals tests

--- ch05/magic_number.py
+++
@@ -1,6 +0,0 @@
-print("try to guess the number, please")
-number = input("please enter a number:\n")
-if number == "five":
-    print(f"the {number} you've entered is correct")
-else:
-    print(f"{number} you've entered is wrong")
---
+++ ch05/voting.py
@@ -0,0 +1,9 @@
+age = 18
+if age == 18:
+    print("You can vote! Congratulations!")
+elif age < 18:
+    print("You're to young for voting! Sorry!")
+elif 18 < age < 100:
+    print("You're too old for voting! Sorry")
+else:
+    print("So you're more than 100? Jeez!")
