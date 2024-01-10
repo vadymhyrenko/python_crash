d12d862785f9823450d91ebfcf0184e685c8c0a0
Vadym Hyrenko <vadym.hyrenko@gmail.com>
2024-01-10 12:05:44 +0200

    aliens added

---
+++ ch06/aliens.py
@@ -0,0 +1,16 @@
+alien_0 = {
+    'color': 'red',
+    'points': '5',
+    'speed': 'slow',
+}
+alien_1 = {
+    'color': 'yellow',
+    'points': '10',
+    'speed': 'medium',
+}
+alien_2 = {
+    'color': 'grey',
+    'points': '15',
+    'speed': 'fast',
+}
+alien = [alien_0, alien_1, alien_2]
