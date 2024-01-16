67151d5722dd0d9e9acb42c991a64f4c749eb914
Vadym Hyrenko <vadym.hyrenko@gmail.com>
2024-01-16 23:21:36 +0200

    tickets added

---
+++ ch07/pizza.py
@@ -0,0 +1,36 @@
+prompt = "\nPlease enter the pizza topping you'd like. Print 'quit' if OK:\n"
+
+### option 1 ###
+# message = ""
+
+# while message != 'quit':
+#     message = input(prompt)
+#
+#     print(f"Adding {message} to your pizza")
+
+### option 2 ###
+# active = True
+# while active:
+#     message = input(prompt)
+#
+#     if message == 'quit':
+#         active = False
+#     else:
+#         print(f"Adding {message} to your pizza")
+
+### option 3 ###
+# while True:
+#     topping = input(prompt)
+#
+#     if topping == 'quit':
+#         break
+#     else:
+#         print(f"Adding {topping} to your pizza")
+
+### option 4 ###
+# while True:
+#     topping = input(prompt)
+#     if topping != 'quit':
+#         print(f"  I'll add {topping} to your pizza.")
+#     else:
+#         break
