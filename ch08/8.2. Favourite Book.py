f9a23794ba82b00b45e9fb16ad718880a063d782
Vadym Hyrenko <vadym.hyrenko@gmail.com>
2024-01-18 20:18:50 +0200

    simple def added

---
+++ ch08/8.2. Favourite Book.py
@@ -0,0 +1,6 @@
+def favourite_book(title):
+    """Collecting the informarion about the book lovers"""
+    print(f"One of my favorite books is {title}")
+
+
+favourite_book(input("Enter the name of you favourite book:\n"))
--- ch08/greeter.py
+++ ch08/greeter.py
@@ -1,6 +1,14 @@
-def greet_user():
+def greet():
     """Display a simple greeting."""
     print("Hello, world!")
 
 
-greet_user()
+greet()
+
+
+def greet_user(username):  # username is the parameter here
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+
+
+greet_user('jesus')  # Jesus is an argument here
