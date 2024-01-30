96e9e537e39ebc03be9cffb188310cb798852b0d
Vadym Hyrenko <vadym.hyrenko@gmail.com>
2024-01-30 23:30:14 +0200

    aliens list added

---
+++ ch06/aliens.py
@@ -0,0 +1,7 @@
+alien_0 = {'color': "blue", 'speed': 'slow', 'points': '5'}
+alien_1 = {'color': "green", 'speed': 'medium', 'points': '10'}
+alien_2 = {'color': "grey", 'speed': 'fast', 'points': '15'}
+
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+    print(alien)
